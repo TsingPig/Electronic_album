a998e861ea5dc9cbb1f022c60b1a24add1cdf0b8
JiRunmin <3048390962@qq.com>
2023-12-27 14:58:18 +0800

    暂存

--- Assets/server.py
+++ Assets/server.py
@@ -46,20 +46,66 @@ def download_file(account, filename):
     
 # 输入用户名，返回对应用户名下的相册列表
 @app.route('/get_folders/<account>', methods=['GET'])
-def get_albums(account):
+def get_folders(account):
     album_path = os.path.join(app.config['UPLOAD_FOLDER'], account)
+    folders = {'folders': []}
     if os.path.exists(album_path):
-        folders = {'folders': []}
         # 遍历用户目录下的所有文件夹
         for folder in os.listdir(album_path):
             # 如果是文件夹，将其加入folders字典中
             if os.path.isdir(os.path.join(album_path, folder)):
                 folders['folders'].append(folder)
-    
         print(folders['folders'])
         return json.dumps(folders)
     else:
-        return 'Account not found', 404
+        return json.dumps(folders)
+
+# 输入用户名和相册名，返回对应相册下的图片数量
+@app.route('/connect_size/<account>/<album_name>', methods=['GET'])
+def connect_size(account, album_name):
+    album_path = os.path.join(app.config['UPLOAD_FOLDER'], account, album_name)
+    if os.path.exists(album_path):
+        photo_size = 0
+        for photo in os.listdir(album_path):
+            if os.path.isfile(os.path.join(album_path, photo)):
+                photo_size += 1
+    
+        print(photo_size)
+        return photo_size
+    else:
+        return 'Album not found', 404
+
+# 输入用户名和相册名以及range: l-r 返回按照时间排序后[l, r]的图片
+@app.route('/get_photos/<account>/<album_name>/<range>', methods=['GET'])
+def get_photos(account, album_name, range):
+    album_path = os.path.join(app.config['UPLOAD_FOLDER'], account, album_name)
+    if os.path.exists(album_path):
+        photos = {'photos': []}
+        # 遍历相册目录下的所有文件
+        for photo in os.listdir(album_path):
+            # 如果是文件，将其加入photos字典中
+            if os.path.isfile(os.path.join(album_path, photo)):
+                photo_ctime = os.path.getctime(os.path.join(album_path, photo))
+                photos['photos'].append((photo, photo_ctime))           
+        photos['photos'].sort(key=lambda x: x[1], reverse=True)
+        # 如果range为all，返回所有图片
+        if range == 'all':
+            print(photos['photos'])
+            a = {}
+            for item in photos.items():
+                a[item.key] = item.value[1]
+            return json.dumps(a)
+            for photo, _ in photos['photos']:
+                send_file(photo)
+        # 否则返回range范围内的图片
+        else:
+            l = int(range.split('-')[0])
+            r = int(range.split('-')[1])
+            return json.dumps(photos['photos'][l:r+1])
+            for photo, _ in photos['photos'][l:r+1]:
+                send_file(photo)
+    else:
+        return 'Album not found', 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
