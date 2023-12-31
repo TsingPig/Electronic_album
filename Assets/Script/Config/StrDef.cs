d11e770c118dcc87c7c9a443b92f6a32b17be39d
TsingPig <1114196607@qq.com>
2024-01-01 01:01:30 +0800

    添加View: MomentView 用于展示动态的详情页面 PhotoWallItem 为动态项 MomentPhotoItem 为动态图片项

--- Assets/Script/Config/StrDef.cs
+++ Assets/Script/Config/StrDef.cs
@@ -4,4 +4,8 @@ public static class StrDef
     public const string PHOTO_ITEM_DATA_PATH = "PhotoItem";
     public const string CREATE_PHOTO_WALL_ITEM_VIEW_DATA_PATH = "CreatePhotoWallItemView";
     public const string PHOTO_UPLOAD_ITEM_DATA_PATH = "PhotoUploadItem";
+    public const string MOMENT_DETAIL_VIEW_DATA_PATH = "MomentDetailView";
+    public const string MOMENT_VIEW_DATA_PATH = "MomentView";
+    public const string MOMENT_PHOTO_ITEM_DATA_PATH = "MomentPhotoItem";
+    public const string PHOTO_WALL_ITEM_DATA_PATH = "PhotoWallItem";
 }
