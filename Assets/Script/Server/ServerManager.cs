6194e4303418263003930a3f52b8e9b6f374a1ae
TsingPig <1114196607@qq.com>
2023-12-31 14:31:44 +0800

    添加功能：相册删除联网

--- Assets/Script/Server/ServerManager.cs
+++ Assets/Script/Server/ServerManager.cs
@@ -321,7 +321,7 @@ public class ServerManager : Singleton<ServerManager>
     /// <returns></returns>
     private IEnumerator DeleteFolder(string folderPath, string folderName, Action<FolderList> callback = null)
     {
-        using(UnityWebRequest www = UnityWebRequest.Post($"{url}/deleta_folder/{folderPath}/{folderName}", ""))
+        using(UnityWebRequest www = UnityWebRequest.Get($"{url}/delete_folder/{folderPath}/{folderName}"))
         {
             yield return www.SendWebRequest();
 
