083897a918ad9d63c8ad2aea7b8828215cbdb606
mcy1123 <1009769254@qq.com>
2024-05-17 12:34:30 +0800

    BBStypeitem绑定

--- Assets/Script/UI/BBSTypeView/IBBSTypeView.cs
+++ Assets/Script/UI/BBSTypeView/IBBSTypeView.cs
@@ -1,7 +1,11 @@
 using Michsky.MUIP;
 using MVPFrameWork;
+using UnityEngine;
+using UnityEngine.UI;
+
 public interface IBBSTypeView : IView
 {
     ButtonManager BtnCreateBBSType { get; set; }
 
+    Transform BBSTypeItemRoot { get; set; }
 }
