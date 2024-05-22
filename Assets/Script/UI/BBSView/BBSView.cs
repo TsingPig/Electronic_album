05da7c7180001d65d8a29b1e19be9e66ab5efce6
mcy1123 <1009769254@qq.com>
2024-05-22 18:28:38 +0800

    创建删除模块的弹窗

--- Assets/Script/UI/BBSView/BBSView.cs
+++ Assets/Script/UI/BBSView/BBSView.cs
@@ -12,23 +12,27 @@ public class BBSView : ViewBase<IBBSPresenter>, IBBSView
 
     private ButtonManager _btnDeleteSection;
 
+    private NotificationManager _notification;
+
     public VerticalLayoutGroup BBSPostItemRoot => _bBSPostItemRoot;
 
     public ButtonManager BtnQuit => _btnQuit;
 
     public ButtonManager BtnDeleteSection => _btnDeleteSection;
 
+    public NotificationManager Notification => _notification;
+
     protected override void OnCreate()
     {
         _bBSPostItemRoot = _root.Find<VerticalLayoutGroup>("GroupPanel/MainPanel/ScrollbarView/Viewport/BBSPostItemRoot");
         _btnQuit = _root.Find<ButtonManager>("GroupPanel/btnQuit");
         _btnDeleteSection = _root.Find<ButtonManager>("GroupPanel/btnDeleteSection");
+        _notification = _root.Find<NotificationManager>("GroupPanel/Notification");
         _btnQuit.onClick.AddListener(() =>
         {
             MVPFrameWork.UIManager.Instance.Quit(ViewId.BBSView);
         });
 
         _btnDeleteSection.onClick.AddListener(_presenter.DeleteSection);
-
     }
 }
