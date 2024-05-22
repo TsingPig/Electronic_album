45a88eb6faaa160206497117518f60c454a65eef
tsingpig <1114196607@qq.com>
2024-05-22 17:34:31 +0800

    清理代码

--- Assets/Script/UI/BBSView/BBSModel.cs
+++ Assets/Script/UI/BBSView/BBSModel.cs
@@ -5,16 +5,9 @@ using System.Collections.Generic;
 public class BBSModel : IBBSModel
 {
 
-
-    //private List<IBBSModel.Moment> _moments;
-
     private IBBSModel.BBS _section;
 
     public IBBSModel.BBS Section { get => _section; set => _section = value; }
-
-    //public List<IBBSModel.Moment> Moments { get => _moments; set => _moments = value; }
-
-    //public List<IBBSModel.Section> Sections { get => _sections; set => _sections = value; }
 
     public void SetModel(IBBSModel.BBS section)
     {
